c383fe6aca83b433e6baf14c864830b5c279ff68
Bartek Iwańczuk <biwanczuk@gmail.com>
2018-12-19 19:43:50 +0100

    migration

--- example.ts
+++
@@ -1,4 +0,0 @@
-import { args } from "deno";
-import parseArgs from "./index.ts";
-
-console.dir(parseArgs(args));
--- tests/whitespace.ts
+++
@@ -1,6 +0,0 @@
-import { test, assertEqual } from "https://deno.land/x/testing/testing.ts";
-import parseArgs from "../index.ts";
-
-test(function whitespaceShouldBeWhitespace() {
-    assertEqual(parseArgs([ '-x', '\t' ]).x, '\t');
-});
